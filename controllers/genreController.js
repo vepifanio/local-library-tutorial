eef85205368194c374f976b7888f5f1289aae837
vepifanio <vepifanio1@gmail.com>
2020-01-31 14:04:37 -0300

    Find all genres in db and List all genres page created

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,8 +1,14 @@
 const Genre = require('../models/genre');
 
 // Display list of all Genre.
-exports.genre_list = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre list');
+exports.genre_list = (req, res, next) => {
+    Genre.find().exec((err, list_genres) => {
+        if (err) {
+            return next(err);
+        }
+
+        res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
+    });
 };
 
 // Display detail page for a specific Genre
