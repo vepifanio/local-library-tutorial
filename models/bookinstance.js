b55d3e36a5b2169326654628ac803195ae5fbcf0
vepifanio <vepifanio1@gmail.com>
2020-01-31 12:07:14 -0300

    Now show the Date Formatted

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +20,10 @@ const BookInstanceSchema = new Schema(
 // Virtual for BookInstanceSchema's URL
 BookInstanceSchema.virtual('url').get(function () {
     return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+    return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
