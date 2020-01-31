cf5d9ea59941cebd9487ec49d5754e1cbfc59aaf
vepifanio <vepifanio1@gmail.com>
2020-01-30 22:11:28 -0300

    BookInstance Controller modified

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,41 +1,41 @@
 const BookInstance = require('../models/bookinstance');
 
 // Display list of all Book Instances
-exports.bookistance_list = (req, res) => {
+exports.bookinstance_list = (req, res) => {
     res.send('NOT IMPLEMENTED: BookInstance list');
 };
 
 // Display detail page for a specific Book Instance
-exports.bookistance_detail = (req, res) => {
+exports.bookinstance_detail = (req, res) => {
     res.sen(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
 };
 
 // Display BookInstance create form on GET
-exports.bookistance_create_get = (req, res) => {
+exports.bookinstance_create_get = (req, res) => {
     res.send('NOT IMPLEMENTED: Book Instance create GET');
 };
 
 // Display BookInstance create on POST
-exports.bookistance_create_post = (req, res) => {
+exports.bookinstance_create_post = (req, res) => {
     res.send('NOT IMPLEMENTED: Book Instance create POST');
 };
 
 // Display BookInstance delete form on GET
-exports.bookistance_delete_get = (req, res) => {
+exports.bookinstance_delete_get = (req, res) => {
     res.send('NOT IMPLEMENTED: Book Instance delete GET');
 };
 
 // Display BookInstance delete on POST
-exports.bookistance_delete_post = (req, res) => {
+exports.bookinstance_delete_post = (req, res) => {
     res.send('NOT IMPLEMENTED: Book Instance delete POST');
 };
 
 // Display BookInstance update form on GET
-exports.bookistance_update_get = (req, res) => {
+exports.bookinstance_update_get = (req, res) => {
     res.send('NOT IMPLEMENTED: Book Instance update GET');
 };
 
 // Display BookInstance update on POST
-exports.bookistance_update_post = (req, res) => {
+exports.bookinstance_update_post = (req, res) => {
     res.send('NOT IMPLEMENTED: Book Instance update POST');
 };
