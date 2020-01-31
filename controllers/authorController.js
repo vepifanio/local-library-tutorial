3d4be6b28e82b1e56bb59606bf00a272e629951e
vepifanio <vepifanio1@gmail.com>
2020-01-31 15:15:59 -0300

    Author detail page created

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
+const async = require('async');
+
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 // Display list of all Author
 exports.author_list = function(req, res) {
@@ -13,8 +16,29 @@ exports.author_list = function(req, res) {
 };
 
 // Display detail page for a specific Author
-exports.author_detail = function(req, res) {
-    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+exports.author_detail = function(req, res, next) {
+    
+    async.parallel({
+        author: callback => {
+            Author.findById(req.params.id).exec(callback);
+        },
+        author_books: callback => {
+            Book.find({ 'author': req.params.id }, 'title summary').exec(callback);
+        }
+    }, 
+    (err, results) => {
+        if (err) {
+            return next(err);
+        }
+
+        if (results.author == null) {
+            const err = new Error('Author not found');
+            err.status = 404;
+            next(err);
+        }
+
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.author_books });
+    });
 };
 
 // Display Author create form on GET
